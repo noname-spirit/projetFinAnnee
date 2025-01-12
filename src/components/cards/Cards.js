8257a1ecf16a182f3198a05146fbf17c5ad060ad
noname <bonjour@noname-spirit.com>
2025-01-12 16:29:46 +0100

    test ok affichage

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,13 +1,25 @@
 /* ajouer une image*/
 
-export default function Cards({ name }) {
+export default function Cards({
+  projectName,
+  imageUrl,
+  imageAlt,
+  soft,
+  memberSkills,
+  members,
+}) {
   return (
-    <div>
-      <div>im</div>
-      <div>
-        <p>project name </p>
-        <p>logiciels </p>
-        <p>participent </p>
+    <div className="cards">
+      <div className="card">
+        <p className=".image">
+          {imageUrl}
+          {imageAlt}
+        </p>
+        <p className="projectName">{projectName} </p>
+
+        <p className="logiciels">{soft} </p>
+        <p className="participent"> {memberSkills}</p>
+        <p className="menbers"> {members}</p>
       </div>
     </div>
   );
