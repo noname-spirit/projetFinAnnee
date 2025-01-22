e6a6910714f2ef6c96227715db90d2d6c58db94c
noname <bonjour@noname-spirit.com>
2025-01-22 18:53:24 +0100

    `Refactor Footer and Sectionsept components with minor styling changes`

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,21 @@
 function Footer() {
   return (
     <div className="">
-      <p className="text-9xl"> LET’S TALK - LET’S TALK</p>
-      <button
-        type="button"
-        class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-      >
-        DEVIS gratuit
-      </button>
-      <button
-        type="button"
-        class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-      >
-        nous contacter
-      </button>
+      <p className="text-9xl mt-36"> LET’S TALK - LET’S TALK</p>
+      <div className="mt-10">
+        <button
+          type="button"
+          class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          DEVIS gratuit
+        </button>
+        <button
+          type="button"
+          class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          nous contacter
+        </button>
+      </div>
 
       <div className="flex space-x-4  justify-self-center mt-36">
         <div>Bonjour@noname-spirit.com</div>
@@ -21,7 +23,9 @@ function Footer() {
           <img class="" src="" alt="logo nns"></img>
         </div>
         <div>
-          <p>Noname-spirit| 11/25 - 804 595 239 00018 © All Rights Reserved</p>
+          <p className=" ">
+            Noname-spirit| 11/25 - 804 595 239 00018 © All Rights Reserved
+          </p>
         </div>
       </div>
     </div>
--- src/components/sectionsept/Sectionsept.jsx
+++ src/components/sectionsept/Sectionsept.jsx
@@ -1,13 +1,13 @@
 function Sectionsept() {
   return (
     <div>
-      <h2 className="text-base font-extrabold">
+      <h2 className="text-4xl font-extrabold">
         Lancez votre <span className="colorred"> projet personnalisé</span> dès
         maintenant !
       </h2>
       <div className="flex">
         <div><img class="" src="" alt="flech"></img></div>
-        <p className="text-base">Demander une proposition gratuite</p>
+        <p className="text-3xl ">Demander une proposition gratuite</p>
       </div>
     </div>
   );
